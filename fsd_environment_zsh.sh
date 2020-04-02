b9df1d9f1a67a1054e941e28143ae8bb8cf101c2
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-03 01:05:42 +1100

    Refactor for ROS Melodic and Ubuntu Bionic

--- fsd_environment_zsh.sh
+++ fsd_environment_zsh.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
-CATKIN_SHELL=bash
+CATKIN_SHELL=zsh
 
-export FSD_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+export FSD_ROOT=$( cd "$(dirname "${(%):-%N}" )" && pwd)
 
-printf "Sourcing fsd_skeleton/devel/setup.bash\n"
+printf "Sourcing fsd_skeleton/devel/setup.zsh\n"
 
 # check whether devel folder exists
-if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
+if [ -f "${FSD_ROOT}/devel/setup.zsh" ]; then
     # source setup.sh from same directory as this file
-    source "${FSD_ROOT}/devel/setup.bash"
+    source "${FSD_ROOT}/devel/setup.zsh"
 else
     source "/opt/ros/melodic/setup.bash"
     printf "You need to build first before you can source\n"
